3cf07954268357e6a7d3ce0ff1608042911593f5
isaac <isaacbr871@gmail.com>
2024-04-23 10:53:49 -0300

    #3728 2° commit ajuste no evento desconto maximo

--- AppPe/AppPe/Model/Lancamento/Behaviors/ValorUnitarioBehaviors.cs
+++ AppPe/AppPe/Model/Lancamento/Behaviors/ValorUnitarioBehaviors.cs
@@ -53,6 +53,9 @@ namespace Xamarin.HLP.Mobile.AppPE.Model.Lancamento.Behaviors
                         await App.Messages.ShowAsync("DESCONTO MÁX DE " + ViewModel.currentModel.currentTabelaPreco.pDescontoMaximo + "% ULTRAPASSADO!");
                     }
                 }
+
+                VerificarValorUnitario(sender, ViewModel);
+
             }
         }
 
@@ -69,7 +72,7 @@ namespace Xamarin.HLP.Mobile.AppPE.Model.Lancamento.Behaviors
                             var ViewModel = ValorUnitarioComImpostosBehaviors.GetViewModelEditar(entry);
                             var _currentItem = ViewModel.currentModel;
 
-                            if(ViewModel.currentModel.currentTabelaPreco == null)
+                            if (ViewModel.currentModel.currentTabelaPreco == null)
                             {
                                 return;
                             }
@@ -99,30 +102,129 @@ namespace Xamarin.HLP.Mobile.AppPE.Model.Lancamento.Behaviors
                                     ViewModel.pDesconto = 0;
                                 }
 
-                                IDescontoValido _objDescValido;
-
-                                _objDescValido = new DescontoValido(pDescMaximo: ViewModel.currentModel.currentTabelaPreco.pDescontoMaximo);
-
-                                IsValid = _objDescValido.ValidarDesconto(pDesconto: ViewModel.pDesconto); 
-                                ((Entry)sender).TextColor = IsValid ? Color.Default : Color.Red;                            
-
-                                if ((_currentItem.ItensGrade?.Count ?? 0) > 0)
-                                {
-                                    foreach (var item in _currentItem.ItensGrade)
-                                    {
-                                        item.vDesconto = ViewModel.vDesconto;
-                                    }
-                                }
-
-                                PedidoVendaCalculos.CalculoValorSubTotal(ViewModel.currentModel);
-                                PedidoVendaCalculos.CalculoValorComissao(ViewModel.currentModel);
-                                PedidoVendaCalculos.CalculoValorSubTotalSemImpostos(ViewModel.currentModel);
-                                ViewModel.currentModel.SetDetalheItem();
+                                VerificarValorUnitario(sender, ViewModel);
                             }
                         }
                     }
             }
         }
+
+        public void VerificarValorUnitario(object sender, EditarItemViewModel ViewModel)
+        {
+            var _currentItem = ViewModel.currentModel;
+
+            IDescontoValido _objDescValido;
+
+            _objDescValido = new DescontoValido(pDescMaximo: ViewModel.currentModel.currentTabelaPreco.pDescontoMaximo);
+
+            IsValid = _objDescValido.ValidarDesconto(pDesconto: ViewModel.pDesconto);
+            ((Entry)sender).TextColor = IsValid ? Color.Default : Color.Red;
+
+            if ((_currentItem.ItensGrade?.Count ?? 0) > 0)
+            {
+                foreach (var item in _currentItem.ItensGrade)
+                {
+                    item.vDesconto = ViewModel.vDesconto;
+                }
+            }
+
+            PedidoVendaCalculos.CalculoValorSubTotal(ViewModel.currentModel);
+            PedidoVendaCalculos.CalculoValorComissao(ViewModel.currentModel);
+            PedidoVendaCalculos.CalculoValorSubTotalSemImpostos(ViewModel.currentModel);
+            ViewModel.currentModel.SetDetalheItem();
+        }
+
+        //private async void bindable_TextChanged(object sender, TextChangedEventArgs e)
+        //{
+        //    if (e.NewTextValue != e.OldTextValue)
+        //    {
+        //        if (e.NewTextValue.Split(',').Count() > 1)
+        //            if (e.NewTextValue.Split(',')[1].Length == 2)
+        //            {
+        //                var entry = sender as Entry;
+        //                if (entry != null)
+        //                {
+        //                    var ViewModel = ValorUnitarioComImpostosBehaviors.GetViewModelEditar(entry);
+        //                    var _currentItem = ViewModel.currentModel;
+
+        //                    if (ViewModel.currentModel.currentTabelaPreco == null)
+        //                    {
+        //                        return;
+        //                    }
+
+        //                    if (ViewModel == null) return;
+
+
+        //                    if (entry.IsFocused) // se estivir focado, eu zero os valores de desconto
+        //                    {
+        //                        var _vTabela = _currentItem.currentTabelaPreco != null ? ViewModel.vUnitarioVenda
+        //                       : ViewModel.vUnitarioVendaComImpostos;
+
+        //                        var _vDesconto = _vTabela - ViewModel.vUnitarioVendaComImpostos;
+
+        //                        if (_vDesconto > 0 && _vTabela > 0)
+        //                        {
+        //                            ViewModel.vDesconto = _vDesconto;
+        //                            double _pDescAux = ((_vDesconto * 100) / _vTabela);
+
+        //                            _pDescAux = Math.Round(value: _pDescAux, digits: 2, mode: MidpointRounding.ToEven);
+
+        //                            ViewModel.pDesconto = _pDescAux;
+        //                        }
+        //                        else
+        //                        {
+        //                            ViewModel.vDesconto = 0;
+        //                            ViewModel.pDesconto = 0;
+        //                        }
+
+        //                        IDescontoValido _objDescValido;
+
+        //                        _objDescValido = new DescontoValido(pDescMaximo: ViewModel.currentModel.currentTabelaPreco.pDescontoMaximo);
+
+        //                        IsValid = _objDescValido.ValidarDesconto(pDesconto: ViewModel.pDesconto);
+        //                        ((Entry)sender).TextColor = IsValid ? Color.Default : Color.Red;
+
+        //                        if (!IsValid)
+        //                        {
+
+        //                            if (App.CurrentAspnetUserModel.objEmpresaAspnetUsersModel.stAdministrador)
+        //                            {
+        //                                if (await App.Messages.ShowConfirmAsync("DESCONTO MÁX DE " + ViewModel.currentModel.currentTabelaPreco.pDescontoMaximo + "% ULTRAPASSADO! DESEJA CONTINUAR?",
+        //                                        "Não", "Sim"))
+        //                                {
+        //                                    ViewModel.vUnitarioVendaComImpostos += ViewModel.vDesconto;
+        //                                    ViewModel.vDesconto = 0;
+        //                                    ViewModel.pDesconto = 0;
+        //                                }
+        //                            }
+
+        //                            else
+        //                            {
+        //                                ViewModel.vUnitarioVendaComImpostos += ViewModel.vDesconto;
+        //                                ViewModel.vDesconto = 0;
+        //                                ViewModel.pDesconto = 0;
+        //                                await App.Messages.ShowAsync("DESCONTO MÁX DE " + ViewModel.currentModel.currentTabelaPreco.pDescontoMaximo + "% ULTRAPASSADO!");
+        //                            }
+        //                        }
+
+
+        //                        if ((_currentItem.ItensGrade?.Count ?? 0) > 0)
+        //                        {
+        //                            foreach (var item in _currentItem.ItensGrade)
+        //                            {
+        //                                item.vDesconto = ViewModel.vDesconto;
+        //                            }
+        //                        }
+
+        //                        PedidoVendaCalculos.CalculoValorSubTotal(ViewModel.currentModel);
+        //                        PedidoVendaCalculos.CalculoValorComissao(ViewModel.currentModel);
+        //                        PedidoVendaCalculos.CalculoValorSubTotalSemImpostos(ViewModel.currentModel);
+        //                        ViewModel.currentModel.SetDetalheItem();
+        //                    }
+        //                }
+        //            }
+        //    }
+        //}
 
         protected override void OnDetachingFrom(Entry bindable)
         {
