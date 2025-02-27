222ffb6a9ea32d3ecd1d02c837bfe85fb20d44c0
isaac <isaacbr871@gmail.com>
2025-02-27 17:38:30 -0300

    #4612 1° commit ajuste ao enviar limite de credito null

---
+++ AppPe/AppPe/View/Converter/Generic/FormatPermiteNullSimboloDecimalConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Xamarin.HLP.Mobile.AppPE.View.Converter.Generic
+{
+    public class FormatPermiteNullSimboloDecimalConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            try
+            {
+                return value.ToCurrencyPermiteNullSimplesPlacesPtBr();
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            int iMax = 0;
+            var valor = (string)(value ?? "");
+
+            int.TryParse((parameter != null ? parameter.ToString() : ""), out iMax);
+            if (iMax > 0)
+            {
+                valor = valor.RetiraCaracterEspecial();
+                if (valor.Length > iMax)
+                {
+                    valor = valor.Substring(0, iMax);
+                    valor = valor.Insert(valor.Length - 2, ",");
+                    return valor.ToDoublePtBr();
+                }
+            }
+            valor = valor.Replace(",", "").Replace(".", "");
+            valor = valor.PadLeft(3, '0');
+            valor = valor.Insert(valor.Length - 2, ",");
+            return valor.ToDoublePtBr();
+        }
+    }
+}
