d4e4a4156a409128b750e21eb6ff98fd27c031d9
isaac <isaacbr871@gmail.com>
2025-04-07 15:13:44 -0300

    #2944 1° commit ajustes meta

--- AppPe/AppPe/ViewModel/Sincronizacao/SincronizacaoNewViewModel.cs
+++ AppPe/AppPe/ViewModel/Sincronizacao/SincronizacaoNewViewModel.cs
@@ -2267,6 +2267,7 @@ namespace Xamarin.HLP.Mobile.AppPE.ViewModel.Sincronizacao
                                 {
                                     App.CurrentAspnetUserModel.objEmpresaAspnetUsersModel.imUsuario = usuario.imUsuario;
                                     App.CurrentAspnetUserModel.objEmpresaAspnetUsersModel.bGravaLocRepresentante = usuario.bGravaLocRepresentante;
+                                    App.CurrentAspnetUserModel.objEmpresaAspnetUsersModel.vMetaCorrente = usuario.vMetaCorrente;
                                     PageHomeNew.ViewModelStatic.AtualizaImagemApp();
                                 }
                             }
