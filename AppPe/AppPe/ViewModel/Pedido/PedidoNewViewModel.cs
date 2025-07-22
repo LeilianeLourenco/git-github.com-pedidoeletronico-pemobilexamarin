e901044e744c36f6f07c3d3f72ae384fa11447d1
isaac <isaacbr871@gmail.com>
2025-07-22 11:30:39 -0300

    Revert "1 commit ajustes no ios"
    This reverts commit 590e74ad6ffb2ef6e41000278ffb2d99cc1565bd.

--- AppPe/AppPe/ViewModel/Pedido/PedidoNewViewModel.cs
+++ AppPe/AppPe/ViewModel/Pedido/PedidoNewViewModel.cs
@@ -1422,7 +1422,7 @@ namespace Xamarin.HLP.Mobile.AppPE.ViewModel.Pedido
                     currentModel.stCadastroMinimoVenda = _configuracoesGerais.stCadastroLimiteVendasEmpresa;
                     currentModel.stCalculoMinimoVenda = _configuracoesGerais.stCalculoLimiteVendasEmpresa;
                     currentModel.bForcarMinimoVendas = _configuracoesGerais.bForcarMinimoVendas;
-                    bExibirValorPorPeso = _configuracoesGerais.bExibirValorPorPeso ?? false;
+                    bExibirValorPorPeso = _configuracoesGerais.bExibirValorPorPeso;
                 }
 
                 if (currentModel.idPedidoVenda > 0)
