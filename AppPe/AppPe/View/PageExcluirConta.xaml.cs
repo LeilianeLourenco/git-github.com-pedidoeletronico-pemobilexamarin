5b27698899a5d75c94a9a61d89ec2159649ea955
Leiliane Pilau <leilianelourenzo@gmail.com>
2026-08-06 17:56:35 -0300

    #060826 1º commit ajustes para permiter exclusão empresa

---
+++ AppPe/AppPe/View/PageExcluirConta.xaml.cs
@@ -0,0 +1,12 @@
+using Xamarin.Forms;
+
+namespace Xamarin.HLP.Mobile.AppPE.View
+{
+    public partial class PageExcluirConta : ContentPage
+    {
+        public PageExcluirConta()
+        {
+            InitializeComponent();
+        }
+    }
+}
